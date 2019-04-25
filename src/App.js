26f9ca63cc822b528be4a98d4bac370a34ddd648
enduroRider <luki.bigos@gmail.com>
2019-04-25 11:37:21 +0200

    progress on search component

--- src/App.js
+++ src/App.js
@@ -4,36 +4,96 @@ import RecipeDetails from "./components/RecipeDetails";
 import RecipeList from "./components/RecipeList";
 
 export default class App extends Component {
-  
   state = {
     recipes: recipes,
-    url: 'https://www.food2fork.com/api/search?key=a669162333d454da2d89f974cdaf5a75'
+    url:
+      "https://www.food2fork.com/api/search?key=a669162333d454da2d89f974cdaf5a75",
+    base_url:
+      "https://www.food2fork.com/api/search?key=a669162333d454da2d89f974cdaf5a75",
+    details_id: 35381,
+    pageIndex: 1,
+    search: "",
+    query: "&q="
   };
 
-  // async getRecipes() {
-  //   try {
-  //     const res = await fetch(this.state.url);
-  //     const jsonData = await res.json();
-  //     this.setState({
-  //       recipes: jsonData.recipes
-  //     });
-  //   }
-  //   catch (err) {
-  //     console.log(err);
-  //   }
-  // }
+  async getRecipes() {
+    try {
+      const res = await fetch(this.state.url);
+      console.log("url for query: " + this.state.url);
+      const jsonData = await res.json();
+      this.setState({
+        recipes: jsonData.recipes
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-  // componentDidMount() {
-  //   this.getRecipes();
-  // }
-  
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  displayPage = index => {
+    switch (index) {
+      default:
+      case 1:
+        return (
+          <RecipeList
+            recipes={this.state.recipes}
+            handleDetails={this.handleDetails}
+            value={this.state.search}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+          />
+        );
+      case 2:
+        return (
+          <RecipeDetails
+            id={this.state.details_id}
+            handleIndex={this.handleIndex}
+          />
+        );
+    }
+  };
+
+  handleIndex = index => {
+    this.setState({
+      pageIndex: index
+    });
+  };
+
+  handleDetails = (index, id) => {
+    this.setState({
+      pageIndex: index,
+      details_id: id
+    });
+  };
+
+  handleChange = e => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { base_url, query, search } = this.state;
+    this.setState(
+      () => {
+        return {
+          url: `${base_url}${query}${search}`,
+          search: ""
+        };
+      },
+      () => {
+        this.getRecipes();
+      }
+    );
+  };
+
   render() {
     return (
-      <React.Fragment>
-        App component
-        <RecipeList recipes={this.state.recipes}/>
-        <RecipeDetails />
-      </React.Fragment>
+      <React.Fragment>{this.displayPage(this.state.pageIndex)}</React.Fragment>
     );
   }
 }
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,11 +4,21 @@ import RecipeSearch from "./RecipeSearch";
 
 export default class RecipeList extends Component {
   render() {
-    const { recipes } = this.props;
+    const {
+      recipes,
+      handleDetails,
+      value,
+      handleChange,
+      handleSubmit
+    } = this.props;
 
     return (
       <React.Fragment>
-        <RecipeSearch />
+        <RecipeSearch
+          value={value}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
         <div className="container my-5">
           <div className="row">
             <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
@@ -18,9 +28,13 @@ export default class RecipeList extends Component {
           <div className="row">
             {// wysweitlic recipe komponent dla wszystkich recipes ze state'u
             recipes.map(recipe => {
-             return(
-              <Recipe key={recipe.recipe_id} recipe={recipe} />
-             ) 
+              return (
+                <Recipe
+                  key={recipe.recipe_id}
+                  recipe={recipe}
+                  handleDetails={handleDetails}
+                />
+              );
             })}
           </div>
         </div>
